05ac9d9ae772272d6fc58fd8ece7ad6cd022f2fc
OrkoHunter <>
2023-12-22 17:22:03 +0000

    Project init

---
+++ trail5/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from trail5.users.models import User
+from trail5.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ trail5/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from trail5.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
